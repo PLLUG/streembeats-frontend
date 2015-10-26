37568d5d195ed61933cb6119621e5d39b8eff1e3
Ivan Lavriv <lavriv92@gmail.com>
2015-10-26 22:04:41 +0200

    added styling to index page

--- app/pods/account/signup/route.js
+++ app/pods/account/signup/route.js
@@ -11,7 +11,7 @@ export default Ember.Route.extend({
 
   signUpWithTwitter() {
     this.get('session').open('twitter').then(() => {
-
+      this.transitionTo('index');
     }, (error) => {
       alert(error);
     });
