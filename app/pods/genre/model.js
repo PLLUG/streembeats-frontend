c6cc09640ec1cc9eefc6344ee6e161d17d86377c
Ivan Lavriv <lavriv92@gmail.com>
2016-02-03 11:29:14 +0200

    Added links

--- app/pods/genre/model.js
+++ app/pods/genre/model.js
@@ -1,5 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+  title: DS.attr('string'),
+  description: DS.attr('string'),
+  wiki_link: DS.attr('string'),
+  created_at: DS.attr('date'),
+  updated_at: DS.attr('date')
 });
--- app/pods/genres/index/route.js
+++ app/pods/genres/index/route.js
@@ -3,5 +3,9 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('genre');
+  },
+
+  setupController() {
+    this.set('genres', this.currentModel);
   }
 });
